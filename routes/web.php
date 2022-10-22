ccbcf390f4e5ac8230cfc5df0be402535336b59e
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-10-23 00:42:46 +0600

    Admin panel auth added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+});
