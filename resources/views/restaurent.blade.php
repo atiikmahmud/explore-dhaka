2b1f0394f8eadaa981b23d96a9f5f613379ba746
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-10-26 20:38:02 +0600

    Page updated

--- resources/views/restaurent.blade.php
+++ resources/views/restaurent.blade.php
@@ -15,12 +15,12 @@
                     
                     <div class="posts bg-light border shadow border mb-3" style="min-height: 200px;">
                         <div class="row">
-                            <div class="col-md-2">
+                            <div class="col-md-3">
                                 <div class="post-image">
                                     <img src="{{ asset('images/post-image.jpg') }}" alt="" height="200px" width="200px">
                                 </div>
                             </div>
-                            <div class="col-md-10" style="padding-left: 30px">
+                            <div class="col-md-9">
                                 <div class="post-details pt-4">
                                     <div class="post-title h4">
                                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, fugit?
@@ -35,33 +35,7 @@
                                 </div>
                             </div>
                         </div>    
-                    </div>
-
-                    <div class="posts bg-light border shadow border" style="min-height: 200px;">
-                        <div class="row">
-                            <div class="col-md-2">
-                                <div class="post-image">
-                                    <img src="{{ asset('images/post-image.jpg') }}" alt="" height="200px" width="200px">
-                                </div>
-                            </div>
-                            <div class="col-md-10" style="padding-left: 30px">
-                                <div class="post-details pt-4">
-                                    <div class="post-title h4">
-                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, fugit?
-                                    </div>
-                                    <div class="post-body pt-3">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam adipisci, accusantium quis ipsa ab perspiciatis.
-                                    </div>
-                                    <div class="post-author pt-4">
-                                        Post by: <strong>Angel Monalisa</strong> <br>
-                                        Post Date: 10 Oct 2022
-                                    </div>
-                                </div>
-                            </div>
-                        </div>    
-                    </div>
-
-                    
+                    </div>                  
 
                 </div>         
             </div>
