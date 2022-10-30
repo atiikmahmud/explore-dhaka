e717154fe2a7b9f7e2d417c0b6caff178d14e0cc
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-10-31 01:07:20 +0600

    Registration route off, logo updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,10 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            {{-- <x-jet-authentication-card-logo /> --}}
+            <div class="logo">
+                <img src="{{url('images/fevicon.png')}}" alt="" height="80px" width="80px">
+            </div>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
