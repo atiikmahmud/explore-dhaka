fb450632190b223bf2972008b11305ccbf44f9dc
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-10-31 01:47:36 +0600

    Dashboard complete

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,122 @@
 <x-app-layout>
-    <h1 class="text-center pt-5">Dashboard</h1>
+    <div class="messages-section mt-3 pb-4">
+        <div class="container">            
+            <div class="card mx-4">             
+                <div class="card-header text-center h3">
+                    Dashboard
+                </div>
+                <div class="card-body" style="min-height: 500px;">
+                    <div class="row">                
+                        
+                        <div class="col-md-3">
+                            <div class="box-section">
+                                <div class="card shadow" style="border-left: 5px solid #e83e8c">
+                                    <div class="card-body row">
+                                        <div class="info col-md-8">
+                                            <div class="box-title">
+                                                <small>Users</small>
+                                            </div>
+                                            <div class="count h3 pt-1" style="color: #e83e8c">
+                                                {{App\Models\User::count()}}
+                                            </div>
+                                            <div class="details">
+                                                <a href="{{ route('profile.show') }}" class="text-decoration-none text-dark">Details</a>
+                                            </div>
+                                        </div>
+                                        <div class="icon col-md-4 pt-3"><i class="fas fa-users" style="font-size: 50px; color:#e83e8c;"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col-md-3">
+                            <div class="box-section">
+                                <div class="card shadow" style="border-left: 5px solid #1cc88a">
+                                    <div class="card-body row">
+                                        <div class="info col-md-8">
+                                            <div class="box-title">
+                                                <small>Total Post</small>
+                                            </div>
+                                            <div class="count h3 pt-1" style="color: #1cc88a">
+                                                {{App\Models\Post::count()}}
+                                            </div>
+                                            <div class="details">
+                                                <a href="{{ route('posts') }}" class="text-decoration-none text-dark">Details</a>
+                                            </div>
+                                        </div>
+                                        <div class="icon col-md-4 pt-3"><i class="fa-regular fa-paste" style="font-size: 50px; color:#1cc88a;"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col-md-3">
+                            <div class="box-section">
+                                <div class="card shadow" style="border-left: 5px solid #4e73df">
+                                    <div class="card-body row">
+                                        <div class="info col-md-8">
+                                            <div class="box-title">
+                                                <small>Comments</small>
+                                            </div>
+                                            <div class="count h3 pt-1" style="color: #4e73df">
+                                                {{App\Models\Comment::count()}}
+                                            </div>
+                                            <div class="details">
+                                                <a href="{{ route('comments') }}" class="text-decoration-none text-dark">Details</a>
+                                            </div>
+                                        </div>
+                                        <div class="icon col-md-4 pt-3"><i class="fa-regular fa-comments" style="font-size: 50px; color:#4e73df;"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col-md-3">
+                            <div class="box-section">
+                                <div class="card shadow" style="border-left: 5px solid #36b9cc">
+                                    <div class="card-body row">
+                                        <div class="info col-md-8">
+                                            <div class="box-title">
+                                                <small>Contact Message</small>
+                                            </div>
+                                            <div class="count h3 pt-1" style="color: #36b9cc">
+                                                {{App\Models\Contact::count()}}
+                                            </div>
+                                            <div class="details">
+                                                <a href="{{ route('messages') }}" class="text-decoration-none text-dark">Details</a>
+                                            </div>
+                                        </div>
+                                        <div class="icon col-md-4 pt-3">
+                                            <i class="fa-regular fa-envelope" style="font-size: 50px; color:#36b9cc;"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>   
+                        
+                        <div class="col-md-12 mt-3">
+                            <div class="box-section">
+                                <div class="card shadow" style="border-left: 5px solid #22df45">
+                                    <div class="card-body row"  style="min-height: 315px;">
+                                        <div class="col-md-6">
+                                            <div class="dashboard-image">
+                                                <img src="{{ asset('images/banner-img.png') }}" alt="" class="d-block mx-auto">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="dashboard-text" style="margin-top: 110px;">
+                                                <h2 class="text-center">Welcome to Explore Dhaka <br> Admin Panel</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
